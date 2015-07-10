e5090bebcfc30c0879c193dfd218e57538c34549
taylordohmen <tdohmen@mail.umw.edu>
2015-07-10 14:17:21 -0400

    Enables user to press ImageButton to search contact list for recipient, rather than manually entering a phone number

--- app/src/main/java/com/mycompany/futuretext/InputActivity.java
+++ app/src/main/java/com/mycompany/futuretext/InputActivity.java
@@ -2,33 +2,34 @@ package com.mycompany.futuretext;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.ContentResolver;
 import android.content.Context;
-//import android.content.pm.PackageManager;
-//import android.content.pm.ResolveInfo;
-//import android.net.Uri;
-//import android.provider.Telephony;
+import android.database.CharArrayBuffer;
+import android.database.ContentObserver;
+import android.database.DataSetObserver;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.widget.EditText;
 import android.view.View;
-//import java.util.Date;
-//import java.text.DateFormat;
-//import java.text.SimpleDateFormat;
 import java.util.Calendar;
-//import java.util.Locale;
-//import android.app.ActivityManager;
 import android.widget.Toast;
+import android.app.LoaderManager;
+import android.database.Cursor;
+import android.content.Loader;
+import android.content.CursorLoader;
 
-public class InputActivity extends ActionBarActivity {
+public class InputActivity extends ActionBarActivity /*implements LoaderManager.LoaderCallbacks<Cursor>*/  {
     public final static String EXTRA_MESSAGE = "com.mycompany.futuretext.MESSAGE";
     public final static String EXTRA_MESSAGEPOST = "com.mycompany.futuretext.MESSAGEPOST";
     public final static String EXTRA_RECIPIENT = "com.mycompany.futuretext.RECIPIENT";
     public final static String EXTRA_DATEANDTIME = "com.mycompany.futuretext.DATEANDTIME";
 
+    static final int PICK_CONTACT_REQUEST = 1; //request code
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,26 +62,10 @@ public class InputActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //called when the user clicks the send button
-    public void sendMessage (View view) {
-
-        EditText getRecipient = (EditText) findViewById(R.id.get_recipients);
-        String recipient = getRecipient.getText().toString();
-        EditText getMessage = (EditText) findViewById(R.id.get_message);
-        String message = getMessage.getText().toString();
-
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        intent.putExtra(EXTRA_MESSAGEPOST, "To: " + recipient + "\n" + message);
-        startActivity(intent);
-
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(recipient, null, message, null, null);
-
-    }
-
     //called when user clicks send later button
     public void sendDelayedMessage(View view) {
 
+        //gets data from input fields
         EditText getRecipient = (EditText) findViewById(R.id.get_recipients);
         String recipient = getRecipient.getText().toString();
         EditText getMessage = (EditText) findViewById(R.id.get_message);
@@ -89,10 +74,10 @@ public class InputActivity extends ActionBarActivity {
         String when = getDateAndTime.getText().toString();
         String messagePost = ("To: " + recipient + "\n" + message + "\n" + when);
 
+        //writes message post to archive file
         MessageLog.toFile(this.getApplicationContext(), MessageLog.STORED_MESSAGES, messagePost);
 
-
-
+        //parses time input
         char[] hourChars = new char[2];
         char[] minuteChars = new char[2];
         when.getChars(0,2,hourChars,0);
@@ -102,34 +87,56 @@ public class InputActivity extends ActionBarActivity {
         int hour = Integer.parseInt(hourString);
         int minute = Integer.parseInt(minuteString);
 
-
-
         Calendar c = Calendar.getInstance();
-
+        //creates unique id for the alarm manager so separate messages stay separate
         String alarmIDString = new StringBuilder().append(c.get(Calendar.HOUR)).append(c.get(Calendar.MINUTE)).append(c.get(Calendar.SECOND)).append(c.get(Calendar.MILLISECOND)).toString();
         int alarmID = Integer.parseInt(alarmIDString);
 
+        //sets calendar to parsed time input
         c.setTimeInMillis(System.currentTimeMillis());
         c.set(Calendar.HOUR_OF_DAY, hour);
         c.set(Calendar.MINUTE, minute);
 
-        Intent intent = new Intent(this, AlarmReceiver.class);
-        //intent.setAction("com.mycompany.futuretext.InputActivity");
-        intent.putExtra(EXTRA_MESSAGE, message);
-        intent.putExtra(EXTRA_RECIPIENT, recipient);
-        intent.putExtra(EXTRA_DATEANDTIME, when);
+        Intent messageIntent = new Intent(this, AlarmReceiver.class);
+        messageIntent.putExtra(EXTRA_MESSAGE, message);
+        messageIntent.putExtra(EXTRA_RECIPIENT, recipient);
+        messageIntent.putExtra(EXTRA_DATEANDTIME, when);
 
-
-        PendingIntent alarmIntent = PendingIntent.getBroadcast(this.getApplicationContext(), alarmID, intent, PendingIntent.FLAG_ONE_SHOT);
-
-
-
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(this.getApplicationContext(), alarmID, messageIntent, PendingIntent.FLAG_ONE_SHOT);
         AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        am.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), alarmIntent);
+        am.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), alarmIntent); //schedules alarm manager for preset time via Calendar c
 
         Toast.makeText(this,("text scheduled for " + hourString + ":" + minuteString), Toast.LENGTH_LONG).show();
-        Toast.makeText(this, alarmIDString, Toast.LENGTH_LONG).show();
-
     }
 
+    //Allows user to access contacts to choose a phone number
+    public void pickContact(View view) {
+
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);//only contacts with phone #s
+        startActivityForResult(pickContactIntent,PICK_CONTACT_REQUEST);
+    }
+
+    //gets data from pickContactIntent, gets number from it and writes in number to get_recipients edittext
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //check request code.
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            //check to see if request was successful
+            if (resultCode == RESULT_OK){
+
+                Uri contactUri = data.getData();
+
+                Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+                cursor.moveToFirst();
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+                EditText getRecipient = (EditText) findViewById(R.id.get_recipients);
+                getRecipient.setText(number);
+                cursor.close();
+
+            }
+        }
+    }
 }
